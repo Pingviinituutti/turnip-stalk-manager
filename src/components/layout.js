fbf3ae2f41cd3e6fd6c155d159c0bd0592722f78
Henrik Hillner <henrik@hillner.fi>
2021-05-05 23:27:41 +0300

    404 redirects to homepage

--- src/components/layout.js
+++ src/components/layout.js
@@ -40,7 +40,7 @@ const Layout = ({ children }) => {
             marginTop: `2rem`,
           }}
         >
-          © {new Date().getFullYear()}, Built with
+          Henrik Hillner © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
         </footer>
